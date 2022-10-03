5c5e25ab371b1b4847dac07b5fc58a71df974c4f
Andrey <a89615076005@yandex.ru>
2022-10-03 11:50:02 +0300

    add task 24

---
+++ Task24/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая принимает на вход число A и выдает сумму чисел от 1 до A
+
+Console.Clear();
+ 
+Console.Write("Введите число А: ");
+int a= int.Parse(Console.ReadLine()!);
+
+int sum = 0;
+
+for (int i = 1; i <= a; i++)
+{
+    sum += i;
+}
+
+Console.Write($"Сумма чисел от 1 до {a} равна {sum}");

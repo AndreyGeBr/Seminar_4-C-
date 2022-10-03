5d832cb08dfd0468ffb550d0902f14b5c27f7852
Andrey <a89615076005@yandex.ru>
2022-10-03 12:49:00 +0300

    add task 28

---
+++ Task28/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая принимает на вход число N и выдает произведение чисел от 1 до N
+
+Console.Clear();
+ 
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int result = 1;
+
+for (int i = 1; i <= n; i++)
+{
+    result *= i;
+}
+
+Console.Write($"Произведение чисел от 1 до {n} равна {result}");

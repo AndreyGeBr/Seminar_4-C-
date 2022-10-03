660e62b11d2d1e54ebbd6a35870fb306e6d0d32d
Andrey <a89615076005@yandex.ru>
2022-10-03 12:21:00 +0300

    add task 26

---
+++ Task26/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа, которая принимает на вход число и выдает количество цифр в числе
+
+Console.Clear();
+ 
+Console.Write("Введите число: ");
+int a = Math.Abs(int.Parse(Console.ReadLine()!));
+CountNumbers ();
+
+void CountNumbers ()
+{
+    int current = a;
+    int count = 0;
+    while (current > 0)
+    {
+        current = current / 10;
+        count++;
+    }
+    Console.WriteLine($"Количество цифр в числе {a} равно {count}");
+}
+
+
+
